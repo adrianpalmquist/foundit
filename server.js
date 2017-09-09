53f970f8a2582d59ab9e6745366a11a703a87e84
Jesper Lund <jeslu246@student.liu.se>
2017-09-09 18:56:07 +0200

    server responds with the object belonging to id in post request

--- server.js
+++ server.js
@@ -1,49 +1,9 @@
-/*
-const http = require('http');
-
-require("jsdom").env("", function(err, window) {
-  if (err) {
-      console.error(err);
-      return;
-  }
-
-  var $ = require("jquery")(window);
-});
-
-http.createServer((request, response) => {
-  console.log('Request received');
-
-  const { headers, method, url } = request;
-  let body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-
-    
-    //Response
-    response.on('error', (err) => {
-      console.error(err);
-    });
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-
-    const responseBody = { headers, method, url, body };
-
-    response.write('response');
-    response.end();
-
-    // END OF NEW STUFF
-  });
-}).listen(8080);
-*/
+//Load dependencies
+var http = require('http');
 
 //test json object (database)
-var database = {
-  
+var database = [
+  {
     "id" : "id-001",
     "name" : "david",
     "location" : {
@@ -52,7 +12,9 @@ var database = {
     },
     "mail" : "jsjsj@sksk.com",
     "phone" : "0456797959",
-  
+  },
+
+  {
     "id" : "id-002",
     "name" : "jesper",
     "location" : {
@@ -61,47 +23,42 @@ var database = {
     },
     "mail" : "aaa@sksk.com",
     "phone" : "0050797959" 
-  
-}
+  }
 
-
-var http = require('http');
+]
 
 function onRequest(req,res){
     var data = "";
     var parsedData;
     console.log('request received');
     
-    req.on('data', function (chunk) {
+    req.on('data', function ( chunk ) {
         data += chunk;
-        parsedData = JSON.parse(data);
+        parsedData = JSON.parse( data );
 
-        console.log("requested id: " + parsedData.id);
+        console.log("Request detected! Requested id: " + parsedData.id);
 
-        /**
-         * Read NFC Tag (look up in db)
-         */
-        
-        if( parsedData.id == "1" ){
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          console.log("user id match found");
-          res.end(JSON.stringify(parsedData));
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end("id not found");
+        //Read NFC Tag (look up in db) ---------------------------------------
+ 
+        //Loop through database object
+        for( key in database ) {
+          for( field in database[key] ) {
+              //console.log(field + ": " + database[key][field])
+            
+              if( database[key][field] == parsedData.id ) {
+                //if id found, return object belonging to the id
+                res.writeHead( 200, {'Content-Type': 'application/json'} );
+                console.log( "user id match found" );
+                res.end( JSON.stringify( database[key] ) );
+              } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end( "id not found" );
+              }
+          }
         }
-                  
-        /*  
-        if(database.hasOwnProperty(parsedData.id)){
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            console.log("user id match found");
-            res.end(parsedData);
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end("id not found");
-        }*/
-        
 
+        //--------------------------------------------------------------------
+                  
     });
  
 }
