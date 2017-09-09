a8a081bc0199c1cdf6a872ecc364063add285e82
adrianpalmquist <adrianpalmquist@gmail.com>
2017-09-09 09:23:38 +0200

    Move android project to folder

---
+++ Android/app/src/main/java/com/esh/group7/foundit/MainActivity.java
@@ -0,0 +1,28 @@
+package com.esh.group7.foundit;
+
+import android.nfc.NfcAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    NfcAdapter mNfcAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        else {
+            Toast.makeText(this, "NFC is available", Toast.LENGTH_LONG).show();
+        }
+    }
+}
